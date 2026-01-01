83fe75943861523226e35c01e4d6de357a56bb81
Victor Svetuskin <vicsvetdev@gmail.com>
2026-01-01 16:56:13 +0200

    Adapt themes for omarchy and clean-up

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -8,7 +8,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -20,28 +20,7 @@ vim.opt.rtp:prepend(lazypath)
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
-    {
-      "catppuccin/nvim",
-      config = function()
-        require("catppuccin").setup({})
-        --vim.cmd.colorscheme("catppuccin")
-      end,
-    },
-    {
-      "folke/tokyonight.nvim",
-      config = function()
-        require("tokyonight").setup({})
-        vim.cmd.colorscheme("tokyonight-night")
-      end,
-      lazy = false,
-      priority = 1000,
-      opts = {},
-    },
-    { import = "config.plugins" },
+    { import = "plugins" }
   },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "tokyonight" } },
-  -- automatically check for plugin updates
   checker = { enabled = false },
 })
---
+++ lua/plugins/theme.lua
@@ -0,0 +1,1 @@
+/home/vicsvet/.config/omarchy/current/theme/neovim.lua
